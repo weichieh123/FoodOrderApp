69c1f266f11bb87fc61e0fdae2a84dd4c3bcbff6
weichieh123 <weichiehhuang@gapp.nthu.edu.tw>
2021-11-27 15:20:23 +0800

    add Modal

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom'
 import classes from './Modal.module.css'
 
 const Backdrop = (props) => {
-  return <div className={classes.backdrop} />
+  return <div className={classes.backdrop} onClick={props.onHideCart} />
 }
 
 const ModalOverlay = (props) => {
@@ -18,7 +18,7 @@ const portalElement = document.getElementById('overlays')
 const Modal = (props) => {
   return (
     <>
-      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(<Backdrop onHideCart={props.onHideCart} />, portalElement)}
       {ReactDOM.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalElement
