61e5128373ba7ad262ed96e69c20e81ee2560c71
weichieh123 <weichiehhuang@gapp.nthu.edu.tw>
2021-11-28 12:28:42 +0800

    購物車按鈕動畫

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,27 +1,44 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import CartContext from '../../store/cart-context'
 import CartIcon from '../Cart/CartIcon'
 import classes from './HeaderCartButton.module.css'
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlight, setBtnIsHighlight] = useState(false)
 
-    const cartCtx = useContext(CartContext)
+  const cartCtx = useContext(CartContext)
+  const { items } = cartCtx
 
-    const numOfCartItems = cartCtx.items.reduce((curNumber, item) => {
-        return curNumber + item.amount
-    }, 0)
+  const numOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount
+  }, 0)
 
-    return (
-        <button className={classes.button} onClick={props.onShowCart}>
-            <span className={classes.icon}>
-                <CartIcon />
-            </span>
-            <span>Your Cart</span>
-            <span className={classes.badge}>
-                {numOfCartItems}
-            </span>
-        </button>
-    )
+  const btnClasses = `${classes.button} ${btnIsHighlight ? classes.bump : ''}`
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return
+    }
+    setBtnIsHighlight(true)
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlight(false)
+    }, 300)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [items])
+
+  return (
+    <button className={btnClasses} onClick={props.onShowCart}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numOfCartItems}</span>
+    </button>
+  )
 }
 
 export default HeaderCartButton
