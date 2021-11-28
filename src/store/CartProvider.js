beabca2d33720731ba994ae72905d578e3be23f2
weichieh123 <weichiehhuang@gapp.nthu.edu.tw>
2021-11-28 12:01:02 +0800

    更新購物車計算方式

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,59 +1,76 @@
-import { useReducer } from "react"
-import CartContext from "./cart-context"
+import { useReducer } from 'react'
+import CartContext from './cart-context'
 
 const defaultCartState = {
-    items: [],
-    totalAmount: 0
+  items: [],
+  totalAmount: 0,
 }
 
 const cartReducer = (state, action) => {
-    if(action.type==='ADD'){
-        // unlike push, concat return a new array
-        const updateItems = state.items.concat(action.item)
-        const updateTotalAmount = state.totalAmount + action.item.price * action.item.amount
-        return {
-            items: updateItems,
-            totalAmount: updateTotalAmount
-        }
-    }
-    if(action.type==='REMOVE'){
-
-    }
-    return(
-        defaultCartState
+  if (action.type === 'ADD') {
+    // unlike push, concat return a new array
+    const updateTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
     )
+    const existingCartItem = state.items[existingCartItemIndex]
+    
+    let updatedItems
+
+    if (existingCartItem) {
+      // 如果要購買購物車已經有的商品
+      const updatedItem = {
+        ...existingCartItem, //原商品名稱
+        amount: existingCartItem.amount + action.item.amount, //原數量+新增數量
+      }
+      updatedItems = [...state.items] //copy原購物車
+      updatedItems[existingCartItemIndex] = updatedItem //只更新重複的商品項目
+    } else {
+      updatedItems = state.items.concat(action.item)
+    }
+    return {
+      items: updatedItems,
+      totalAmount: updateTotalAmount,
+    }
+  }
+  if (action.type === 'REMOVE') {
+  }
+  return defaultCartState
 }
 
-
 const CartProvider = (props) => {
-    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState)
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  )
 
-    const addItemToCartHandler = (item) => {
-        dispatchCartAction({
-            type: 'ADD', 
-            item: item
-        })
-    }
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({
+      type: 'ADD',
+      item: item,
+    })
+  }
 
-    const removeItemFromCartHandler = (id) => {
-        dispatchCartAction({
-            type: 'REMOVE', 
-            id: id
-        })
-    }
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({
+      type: 'REMOVE',
+      id: id,
+    })
+  }
 
-    const cartContext = {
-        items: cartState.items,
-        totalAmount: cartState.totalAmount,
-        addItem: addItemToCartHandler,
-        removeItem: removeItemFromCartHandler
-    }
+  const cartContext = {
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+  }
 
-    return(
-        <CartContext.Provider value={cartContext}>
-            {props.children}
-        </CartContext.Provider>
-    )
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  )
 }
 
 export default CartProvider
