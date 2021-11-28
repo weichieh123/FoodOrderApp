fa2defa96ff3ce302a2dd57ad35f09332ff002de
weichieh123 <weichiehhuang@gapp.nthu.edu.tw>
2021-11-28 12:15:32 +0800

    remove cart item

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -9,15 +9,14 @@ const defaultCartState = {
 const cartReducer = (state, action) => {
   if (action.type === 'ADD') {
     // unlike push, concat return a new array
-    const updateTotalAmount =
+    const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount
     const existingCartItemIndex = state.items.findIndex(
       (item) => item.id === action.item.id
     )
     const existingCartItem = state.items[existingCartItemIndex]
-    
-    let updatedItems
 
+    let updatedItems
     if (existingCartItem) {
       // 如果要購買購物車已經有的商品
       const updatedItem = {
@@ -31,10 +30,27 @@ const cartReducer = (state, action) => {
     }
     return {
       items: updatedItems,
-      totalAmount: updateTotalAmount,
+      totalAmount: updatedTotalAmount,
     }
   }
   if (action.type === 'REMOVE') {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    )
+    const existingCartItem = state.items[existingCartItemIndex]
+    const updatedTotalAmount = state.totalAmount - existingCartItem.price
+    let updatedItems 
+    if(existingCartItem.amount === 1) {
+        updatedItems = state.items.filter(item => item.id !== action.id)
+    } else {
+        const updatedItem = {...existingCartItem, amount: existingCartItem.amount -1}
+        updatedItems = [...state.items]
+        updatedItems[existingCartItemIndex] = updatedItem
+    }
+    return {
+        items: updatedItems,
+        totalAmount: updatedTotalAmount
+    }
   }
   return defaultCartState
 }
