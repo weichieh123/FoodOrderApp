b6cd1176b6cdc959b9993b256626f564b226c67c
weichieh123 <weichiehhuang@gapp.nthu.edu.tw>
2021-12-06 22:06:53 +0800

    handle fetch error

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -7,6 +7,7 @@ import MealItem from './MealItem/MealItem'
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+  const [httpError, setHttpError] = useState(null)
 
   /*
     如何在useEffect裡使用async await
@@ -14,10 +15,17 @@ const AvailableMeals = () => {
     Instead, the function you pass to useEffect may return a cleanup function which can be executed.
     That cleanup function should run synchronously though.
     It should not return a promise
+
+    可以刪掉fetch url的.json來測試error msg
   */
   useEffect(() => {
     const fetchMeals = async () =>{
       const res = await fetch('https://food-order-55733-default-rtdb.firebaseio.com/meals.json')
+
+      if(!res.ok) {
+        throw new Error('Something went wrong!')
+      }
+
       const resData = await res.json()
 
       // change firebase data from obj to array
@@ -33,14 +41,26 @@ const AvailableMeals = () => {
       setMeals(loadedMeals)
       setIsLoading(false)
     }
-    fetchMeals()
-
+    
+      fetchMeals().catch(error => {
+        setIsLoading(false)
+        setHttpError(error.message)
+      })
+      
   }, [])
 
   if (isLoading){
     return (
       <section className={classes.MealsLoading}>
         <p>Loading</p>
+      </section>
+    )
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
       </section>
     )
   }
